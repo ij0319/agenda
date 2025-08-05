d955aaac56685f759226daa44dd3bd4a8aa69e21
Isaias JR <isaiasjunior.1711@gmail.com>
2025-08-05 18:54:40 -0300

    Adição da classe ContatoRepository

---
+++ agenda/src/main/java/com/agenda/agenda/repository/ContatoRepository.java
@@ -0,0 +1,6 @@
+package com.agenda.agenda.repository;
+import com.agenda.agenda.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+}
